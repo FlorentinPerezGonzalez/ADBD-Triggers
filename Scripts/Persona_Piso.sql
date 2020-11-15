f9250c2dcf073d246d19b4704738c107858ec949
Florentín Pérez González <56076163+alu0101100654@users.noreply.github.com>
2020-11-15 18:26:50 +0000

    Scripts y Readme

---
+++ Scripts/Persona_Piso.sql
@@ -0,0 +1,7 @@
+CREATE DEFINER = CURRENT_USER TRIGGER `mydb`.`Persona_Piso_BEFORE_INSERT` BEFORE INSERT ON `Persona_Piso` FOR EACH ROW
+BEGIN
+	IF (NEW.DNI IN (SELECT DNI
+    FROM Persona_Vivienda)) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Persona ya viviendo en una vivienda';
+	END IF;
+END
