54770209769175d0de04aff8c3815eea33403620
Florentín Pérez González <56076163+alu0101100654@users.noreply.github.com>
2020-11-16 18:24:43 +0000

    Delete Persona_Vivienda.sql

--- Scripts/Persona_Vivienda.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER = CURRENT_USER TRIGGER `mydb`.`Persona_Vivienda_BEFORE_INSERT` BEFORE INSERT ON `Persona_Vivienda` FOR EACH ROW
-BEGIN
-    IF (NEW.DNI IN (SELECT DNI
-    FROM Persona_Piso)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Persona viviendo en un piso';
-    END IF;
-END
